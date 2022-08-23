8f3f2a7e362175eb6cefa0699d6ac7d8e1cad8bd
siddeshanm <83800249+siddeshanm@users.noreply.github.com>
2022-08-23 15:07:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const form=document.querySelector("#searchForm")
+form.addEventListener('submit',async function(e){
+    e.preventDefault();
+    const key=form.elements.query.value;
+    const config={ params:{q:key}}
+  
+   const res= await  axios.get(`https://api.tvmaze.com/search/shows?`,config)
+  
+   makeImages(res.data)
+   form.elements.query.value=''
+})
+const makeImages=(shows)=>{
+for(let result of shows)
+{
+    const img=document.createElement('IMG')
+   img.src=result.show.image.medium
+   document.body.append(img)
+}
+}
+const clearImages=(shows)=>{
+    for(let result of shows)
+    {
+        const img=document.createElement('IMG')
+       img.src=''
+       document.body.append(img)
+    }
+    }
